f94027aadf166859dc67d434e9600bdb1cc62458
陈雨 <chenyu_13820592380@126.com>
2021-10-19 01:12:00 +0800

    首页

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import bg from './bg-star.jpg';
 
 const App = () => {
   const [page, setPage] = React.useState(1);
   const pageRef = React.useRef();
 
   const handleWheel = (e) => {
+    // console.log(e)
     if (e.deltaY > 0 && pageRef.current < 3) {
       setPage(pageRef.current + 1);
     }
@@ -30,16 +32,24 @@ const App = () => {
       <div className="container" style={{
         top: `${-(page - 1) * 100}vh`
       }}>
-        <div className="page">
-          第一屏内容
+        <div className="page one">
+          <div className="picture-container">
+            <img className="picture" src={bg}/>
+          </div>
+          <div className="title">
+            雨霏霏
+          </div>
+          <div className="subtitle">
+            一个不太美、超笨的、爱吃的程序媛~
+          </div>
           <button onClick={() => setPage(2)}>向下滚</button>
         </div>
-        <div className="page">
+        <div className="page two">
           第二屏内容
           <button onClick={() => setPage(1)}>向上滚</button>
           <button onClick={() => setPage(3)}>向下滚</button>
         </div>
-        <div className="page">
+        <div className="page three">
           第三屏内容
           <button onClick={() => setPage(2)}>向上滚</button>
         </div>
